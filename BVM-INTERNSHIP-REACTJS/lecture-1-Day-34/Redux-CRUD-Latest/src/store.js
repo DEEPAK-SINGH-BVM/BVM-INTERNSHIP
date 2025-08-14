a38c300ef829ccda43ecc57f29a3193dfcb3036f
DEEPAK-SINGH-BVM <deepaksingh.bvminfotech@gmail.com>
2025-08-14 12:37:29 +0530

    Redux-CRUD-COMPELTE

--- BVM-INTERNSHIP-REACTJS/lecture-1-Day-34/Redux-CRUD-Latest/src/store.js
+++ BVM-INTERNSHIP-REACTJS/lecture-1-Day-34/Redux-CRUD-Latest/src/store.js
@@ -1,4 +1,4 @@
-import reducers from "./reducers";
+// import reducers from "./reducers";
 // import { configureStore } from "@reduxjs/toolkit";
 // import { persistStore, persistReducer } from "redux-persist";
 // import storage from "redux-persist/lib/storage";
@@ -22,6 +22,50 @@ import reducers from "./reducers";
 //   let persistor = persistStore(store)
 //   return { store , persistor}
 // }
+
+
+
+// import reducers from "./reducers";
+// // import { configureStore } from "@reduxjs/toolkit";
+// // import { persistStore, persistReducer } from "redux-persist";
+// // import storage from "redux-persist/lib/storage";
+// // import rootReducer from "./reducers/crudReducers";
+// // import {combineReducers } from 'redux'
+
+// // // const store = configureStore({
+// // //   reducer: {
+// // //     counter: reducers,
+// // //   },
+// // // });
+// // const persistConfig = {
+// //   key: "root",
+// //   storage,
+// // };
+
+// // const reducerCombine = combineReducers ({
+// //   other:reducers
+// // })
+
+// // export default persistReducer(persistConfig, reducerCombine);
+// // const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// // export default store;
+// // export default () => {
+// //   let store = configureStore(persistedReducer);
+// //   let persistor = persistStore(store);
+// //   return { store, persistor };
+// // };
+// // export const store = configureStore(persistedReducer);
+// // export const persistor = persistStore(store)
+
+// import { combineReducers } from "redux";
+// import reducers from "./reducers";
+
+// const rootReducer = combineReducers({
+//   user: reducers,
+// });
+
+// export default rootReducer;
 
 import { configureStore } from "@reduxjs/toolkit";
 import { persistStore, persistReducer } from "redux-persist";
